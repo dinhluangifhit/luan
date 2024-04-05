f2ca38a1ba374da3f65913f943f89414c14671be
Hnhung692 <nguyenthihongnhung060902@gmail.com>
2024-04-05 17:45:18 +0700

    change

---
+++ MyappVeXe/app/src/main/java/com/example/myappvexe/Customer/HomePage.java
@@ -0,0 +1,16 @@
+package com.example.myappvexe.Customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myappvexe.R;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
